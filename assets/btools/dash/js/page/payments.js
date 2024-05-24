d04f1a168b4e0a5951127cdf9c174fbd13c92432
Fahmi Hidayatulloh <fahmicog@gmail.com>
2024-05-24 07:11:28 +0700

    new

--- assets/btools/dash/js/page/payments.js
+++ assets/btools/dash/js/page/payments.js
@@ -1,72 +1,63 @@
-var type = 'Elite',
-  pricePacket = 50000,
-  itemLoader = '<div class="d-flex justify-content-center"><div class="sk-wave sk-primary"><div class="sk-wave-rect"></div><div class="sk-wave-rect"></div><div class="sk-wave-rect"></div><div class="sk-wave-rect"></div><div class="sk-wave-rect"></div></div></div>';
+$(function() {
+  var itemLoader = '<div class="d-flex justify-content-center"><div class="sk-wave sk-primary"><div class="sk-wave-rect"></div><div class="sk-wave-rect"></div><div class="sk-wave-rect"></div><div class="sk-wave-rect"></div><div class="sk-wave-rect"></div></div></div>';
 
-$('input[type=radio][name=type]').on('change', function() {
-  type = $(this).val()
-  if (type == 'Elite') {
-    pricePacket = 50000;
-  } else {
-    pricePacket = 100000;
-  }
-});
-
-$('#btWallet').click(function(){
-  $(".paymentf").block({ message: itemLoader, css: { backgroundColor: 'transparent', border: '0'}, overlayCSS: { backgroundColor:"#fff", opacity:.8 } });
+$('#btWallet').click(function () {
+  $('.payment-form').block({ message: itemLoader, css: { backgroundColor: 'transparent', border: '0' }, overlayCSS: { backgroundColor: '#fff', opacity: 0.8 } });
   $.ajax({
     data: {
-      username: mdat.name,
-      number: mdat.number,
-      type,
-      total: pricePacket,
+      type: $('#type').val(),
       payment_method: 'E-Wallet'
     },
     url: '/a/payment/fpay',
     type: 'POST',
     success: function (d) {
-      $(".paymentf").unblock()
-      fpay(d.token, {
-        onSuccess: function(res){
-          paymentProcess(res);
-        },
-        onPending: function(){
-          Swal.fire({ title: "Wait!", text: 'Wating your payment!', icon: "warning", customClass: { confirmButton: "btn btn-primary" }, buttonsStyling: !1 });
-        },
-        onError: function(){
-          Swal.fire({ title: "Error!", text: 'Payment failed!!', icon: "error", customClass: { confirmButton: "btn btn-primary" }, buttonsStyling: !1 });
-        },
-        onClose: function(){
-          Swal.fire({ title: "Upss!", text: 'You closed the popup without finishing the payment!', icon: "warning", customClass: { confirmButton: "btn btn-primary" }, buttonsStyling: !1 });
-        }
-      });
+      $('.payment-form').unblock();
+      handlerPay(d.token);
     },
     error: function (e) {
-      $(".paymentf").unblock()
+      $('.payment-form').unblock();
       const msg = e.responseJSON.msg;
-      Swal.fire({ title: 'Upss!', text: msg ? msg : 'There is an error!', icon:'error', customClass:{ confirmButton:'btn btn-primary' }, buttonsStyling: !1 });
+      Swal.fire({ title: 'Upss!', text: msg ? msg : 'There is an error!', icon: 'error', customClass: { confirmButton: 'btn btn-primary' }, buttonsStyling: !1 });
     }
   });
-})
-  
-function paymentProcess(res){
-  $.blockUI({ message: itemLoader, css: { backgroundColor: 'transparent', border: '0'}, overlayCSS: { backgroundColor:"#fff", opacity:.8 } });
+});
+
+function handlerPay(token) {
+  fpay(token, {
+    onSuccess: function (res) {
+      paymentProcess(res);
+    },
+    onPending: function () {
+      Swal.fire({ title: 'Wait!', text: 'Wating your payment!', icon: 'warning', customClass: { confirmButton: 'btn btn-primary' }, buttonsStyling: !1 });
+    },
+    onError: function () {
+      Swal.fire({ title: 'Error!', text: 'Payment failed!!', icon: 'error', customClass: { confirmButton: 'btn btn-primary' }, buttonsStyling: !1 });
+    },
+    onClose: function () {
+      Swal.fire({ title: 'Upss!', text: 'You closed the popup without finishing the payment!', icon: 'warning', customClass: { confirmButton: 'btn btn-primary' }, buttonsStyling: !1 });
+    }
+  });
+}
+
+function paymentProcess(res) {
+  $.blockUI({ message: itemLoader, css: { backgroundColor: 'transparent', border: '0' }, overlayCSS: { backgroundColor: '#fff', opacity: 0.8 } });
   $.ajax({
     data: {
-      user: mdat.name,
-      orderId: res.data.order_id,
-      payment_method: res.data.paym
+      order_id: res.transaction.order_id,
+      payment_method: res.transaction.payment_method
     },
     url: '/a/payment/process',
     type: 'POST',
     success: function (d) {
       $.unblockUI();
-      Swal.fire({ title: "Good job!", text: 'Your payment has been successful 🎉', icon: "success", customClass: { confirmButton: "btn btn-primary" }, buttonsStyling: !1 })
-        .then(() => location.reload())
+      Swal.fire({ title: 'Good job!', text: 'Your payment has been successful 🎉', icon: 'success', customClass: { confirmButton: 'btn btn-primary' }, buttonsStyling: !1 }).then(() => location.reload());
     },
     error: function (e) {
       $.unblockUI();
       const msg = e.responseJSON.msg;
-      Swal.fire({ title: 'Upss!', text: msg ? msg : 'There is an error!', icon:'error', customClass:{ confirmButton:'btn btn-primary' }, buttonsStyling: !1 });
+      Swal.fire({ title: 'Upss!', text: msg ? msg : 'There is an error!', icon: 'error', customClass: { confirmButton: 'btn btn-primary' }, buttonsStyling: !1 });
     }
   });
 }
+
+})
